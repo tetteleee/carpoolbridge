fabda90d7d10619ebb6a848e0c15ee56b2a8f763
Kenshiro Ofune <tetteleeevzeeedreeeggeeektm@gmail.com>
2026-07-16 21:43:07 +0900

    不要ファイルを削除

--- src/mocks/destinations.ts
+++
@@ -1,7 +0,0 @@
-import type { Destination } from "../types";
-
-export const mockDestinations: Destination[] = [
-  { id: "d1", name: "○○小学校" },
-  { id: "d2", name: "△△グラウンド" },
-  { id: "d3", name: "△△球場" },
-];
